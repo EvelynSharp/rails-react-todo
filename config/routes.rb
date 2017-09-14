a85fb9cedacdf671a1f3b89d0a5cd8532d31e1c1
Evelyn Sharp <yihongshi90@gmail.com>
2017-09-14 15:31:34 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
   namespace :api do
     resources :items, except: [:new, :edit]
   end
-
+  get '*other', to: 'static#index'
 end
-
-get '*other', to: 'static#index'
